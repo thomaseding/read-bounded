e5ce613366d6c36985eb7de06bd298c769910f42
Thomas Eding <thomaseding@gmail.com>
2015-05-14 22:11:38 -0700

    Exposed helper function and added documentation

--- src/Text/Read/Bounded.hs
+++ src/Text/Read/Bounded.hs
@@ -1,6 +1,7 @@
 module Text.Read.Bounded (
     BoundedRead(..),
-    ReadBounded(..)
+    ReadBounded(..),
+    readBoundedInteger,
 ) where
 
 
@@ -9,10 +10,11 @@ import Data.Word
 import Text.Read (readMaybe)
 
 
+-- | Information about a bounded read.
 data BoundedRead a
-    = NoRead
-    | ExactRead a
-    | ClampedRead a
+    = NoRead -- ^ The read failed.
+    | ExactRead a -- ^ The value was successfully read exactly, and did not have to be clamped to a narrower representation.
+    | ClampedRead a -- ^ The value was successfully read, but had to be clamped to a narrower representation because its value was too wide.
     deriving (Show, Read, Eq, Ord)
 
 
@@ -38,14 +40,39 @@ clamp x = if x < minInteger
         maxInteger = toInteger maxNum
 
 
-readClamped :: (Bounded a, Read a, Integral a) => String -> BoundedRead a
-readClamped str = case readMaybe str of
+-- | Reads a clamped value for any integer type with the given class constraints.
+-- Useful for implementing a 'ReadBounded' instance or avoiding one.
+readBoundedInteger :: (Bounded a, Read a, Integral a) => String -> BoundedRead a
+readBoundedInteger str = case readMaybe str of
     Nothing -> NoRead
     Just x -> case clamp x of
         Exact y -> ExactRead y
         Clamped y -> ClampedRead y
 
 
+-- | Much like the 'Prelude.Read' class, but will return (possibly) clamped values.
+--
+-- Typical instances of this class will clamp against 'Prelude.Bounded.minBound' and 'Prelude.Bounded.maxBound'
+--
+-- This class is designed to avoid inconsistency problems such as the following:
+--
+-- >>> read "999999999999999999999" :: Int
+-- 3875820019684212735
+-- >>> read "9999999999999999999999" :: Int
+-- 1864712049423024127
+--
+-- Using this class, the results are predictable and precise:
+--
+-- >>> readBounded "999999999999999999999" :: BoundedRead Int
+-- ClampedRead 9223372036854775807
+-- >>> readBounded "9999999999999999999999" :: BoundedRead Int
+-- ClampedRead 9223372036854775807
+-- >>> readBounded "9223372036854775807" :: BoundedRead Int
+-- ExactRead 9223372036854775807
+-- >>> readBounded "1337" :: BoundedRead Int
+-- ExactRead 1337
+-- >>> readBounded "xxx" :: BoundedRead Int
+-- NoRead
 class ReadBounded a where
     readBounded :: String -> BoundedRead a
 
@@ -55,43 +82,43 @@ instance ReadBounded Integer where
 
 
 instance ReadBounded Int where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Int8 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Int16 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Int32 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Int64 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Word where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Word8 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Word16 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Word32 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 instance ReadBounded Word64 where
-    readBounded = readClamped
+    readBounded = readBoundedInteger
 
 
 
